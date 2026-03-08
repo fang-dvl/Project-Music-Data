5b02d03836f6c598d0bb8f6ed8dcccf5a4388dd8
Fanglai <laura@FLMacBook-Air.local>
2026-03-08 23:13:10 +0100

    using 9 functions

--- Project-Music-Data/common.mjs
+++ Project-Music-Data/common.mjs
@@ -1,3 +1,167 @@
-import { getUserIDs } from "./data.mjs";
+import { getSong } from "./data.mjs";
+import { getListenEvents } from "./data.mjs";
 
-export const countUsers = () => getUserIDs().length;
+//get songs that played everyday
+export function checkEveryDaySongs(user) {
+  const events = getListenEvents(user);
+  const songsByDay = {};
+  events.forEach((e) => {
+    const day = e.timestamp.slice(0, 10);
+    if (!songsByDay[day]) {
+  songsByDay[day] = new Set();
+  }
+  songsByDay[day].add(getSong(e.song_id).title);
+  });
+  const allDays = Object.keys(songsByDay);
+  const allSongs = new Set(events.map((e) => getSong(e.song_id).title));
+  const result = {};
+  allSongs.forEach((song) => {
+    result[song] = allDays.every((day) => songsByDay[day].has(song));
+  });
+  return Object.keys(result);
+}
+
+//get the most played song by number of listens
+export function getMostPlayedSongByListens(user) {
+  const events = getListenEvents(user);
+  const listens = {};
+  events.forEach((e) => {
+    let songTitle=getSong(e.song_id).title
+    listens[songTitle] = (listens[songTitle] || 0) + 1;
+  });
+  const maxListens = Math.max(...Object.values(listens));
+  const mostPlayedSongs = Object.keys(listens).filter(
+    (song) => listens[song] === maxListens,
+  );
+  return mostPlayedSongs;
+}
+
+//get the most played song by listening time
+export function getMostPlayedSongByTime(user) {
+  const events = getListenEvents(user);
+  const listens = {};
+  let maxTime = 0;
+  let mostPlayedSong = null;
+  events.forEach((e) => {
+    let songTitle=getSong(e.song_id).title
+    listens[songTitle] = (listens[songTitle] || 0) + 1;;
+    const count = listens[songTitle] || 0;
+    const totalTime = count * getSong(e.song_id).duration_seconds;
+    if (totalTime > maxTime) {
+      maxTime = totalTime;
+      mostPlayedSong = songTitle;
+    } });
+  return mostPlayedSong;
+}
+
+//get the most played artist by number of listens
+export function getMostPlayedArtistByListens(user) {
+  const listens = {};
+  const artists = {};
+  const events = getListenEvents(user);
+  events.forEach((e) => {
+    listens[e.song_id] = (listens[e.song_id] || 0) + 1;
+    let artName = getSong(e.song_id).artist;
+    artists[artName] = (artists.artName || 0) + listens[e.song_id];
+  });
+  const maxListens = Math.max(...Object.values(artists));
+  const mostPlayedArtist = Object.keys(artists).filter(
+    (artist) => artists[artist] === maxListens,
+  );
+  return mostPlayedArtist;
+}
+
+//get the most played artist by listening time
+export function getMostPlayedArtistByTime(user) {
+  const listens = {};
+  let artists = {};
+  const events = getListenEvents(user);
+  events.forEach((e) => {
+    listens[e.song_id] = (listens[e.song_id] || 0) + 1;
+    const count = listens[e.song_id] || 0;
+    const totalTime = count * getSong(e.song_id).duration_seconds;
+    artists[getSong(e.song_id).artist] = (artists[getSong(e.song_id).artist] || 0) + totalTime;
+  });
+  let maxTime = 0;
+  let mostPlayedArtist = null;
+  for (let artist in artists) {
+    if (artists[artist] > maxTime) {
+      maxTime = artists[artist];
+      mostPlayedArtist = artist;
+    }
+  }
+  return mostPlayedArtist;
+}
+
+//get most played song on Friday night by number of listens
+export function getMostPlayedSongFridayByListens(user) {
+  let fridayListens = {};
+  let mostPlayedSong = null;
+  let maxListens = 0;
+  const events = getListenEvents(user);
+  events.forEach((e) => {
+    const date = new Date(e.timestamp);
+    const daysOfWeek = date.getDay();
+    if (daysOfWeek === 5) {
+      fridayListens[e.song_id] = (fridayListens[e.song_id] || 0) + 1;
+      if (fridayListens[e.song_id] > maxListens) {
+        maxListens = fridayListens[e.song_id];
+        mostPlayedSong = e.song_id;
+      }
+    }
+  });
+  return mostPlayedSong;
+}
+
+//get most played song on Friday night by listening time
+export function getMostPlayedSongFridayByTime(user) {
+  const events = getListenEvents(user);
+  let fridayListens = {};
+  let mostPlayedSong = null;
+  let maxTime = 0;
+  let totalTime = 0;
+  events.forEach((e) => {
+    const date = new Date(e.timestamp);
+    const daysOfWeek = date.getDay();
+    if (daysOfWeek === 5) {
+      fridayListens[e.song_id] = (fridayListens[e.song_id] || 0) + 1;
+      totalTime =
+        fridayListens[e.song_id] * getSong(e.song_id).duration_seconds;
+      if (totalTime > maxTime) {
+        maxTime = totalTime;
+        mostPlayedSong = e.song_id;
+      }
+    }
+  });
+  return mostPlayedSong;
+}
+
+//get 3 most played genres by number of listens
+export function getMostGenresByListens(user) {
+  const events = getListenEvents(user);
+  let genreListens = {};
+  events.forEach((e) => {
+    genreListens[getSong(e.song_id).genre] =
+      (genreListens[getSong(e.song_id).genre] || 0) + 1;
+  });
+  const result = Object.entries(genreListens).sort((a, b) => b[1] - a[1]);
+  const mostPlayedGenres = result.slice(0, 3).map((genre) => genre[0]);
+  return mostPlayedGenres;
+}
+
+//get most played song in a row and how many times
+export function getSongInARow(user) {
+  const events = getListenEvents(user);
+  let songsInRow = {};
+  for (let i = 1; i < events.length; i++) {
+    if (events[i].song_id === events[i - 1].song_id) {
+      songsInRow[events[i].song_id] =
+        (songsInRow[events[i].song_id] || 1) + 1;
+    }
+  }
+  if (Object.keys(songsInRow).length === 0) {
+    return ["No song played in a row", 0];
+  }
+  const result = Object.entries(songsInRow).sort((a, b) => b[1] - a[1]);
+  return result[0];
+}
--- Project-Music-Data/script.mjs
+++ Project-Music-Data/script.mjs
@@ -3,9 +3,87 @@
 // You can delete the contents of the file once you have understood how it works.
 // Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
 // You can't open the index.html file using a file:// URL.
-
-import { countUsers } from "./common.mjs";
+import { getUserIDs } from "./data.mjs";
+import { getListenEvents,getSong } from "./data.mjs";
+import { checkEveryDaySongs } from "./common.mjs";
+import { getMostPlayedSongByListens,getMostPlayedSongByTime,} from "./common.mjs";
+import { getMostPlayedArtistByListens,getMostPlayedArtistByTime,} from "./common.mjs";
+import { getMostPlayedSongFridayByListens,getMostPlayedSongFridayByTime,} from "./common.mjs";
+import { getMostGenresByListens, getSongInARow } from "./common.mjs";
 
 window.onload = function () {
-  document.querySelector("body").innerText = `There are ${countUsers()} users`;
+  selectAnUser();
 };
+
+//select an user
+function selectAnUser() {
+  const userSelectBar = document.getElementById("user-source");
+  getUserIDs().forEach((user) => {
+    const option = document.createElement("option");
+    option.textContent = `User ${user}`;
+    option.value = user;
+    userSelectBar.append(option);
+  });
+  userSelectBar.addEventListener("change", (event) => {
+    const selectedUser = event.target.value;
+    createUserPage(selectedUser);
+  });
+}
+
+//create a page for selected user
+const main = document.createElement("main");
+function createUserPage(user) {
+  main.innerHTML = "";
+  if (getListenEvents(user).length === 0) {
+    main.innerHTML = "This user listens no songs.";
+    document.body.appendChild(main);
+  } 
+  else {
+    const everydaySong = document.createElement("p");
+    everydaySong.id = "everyday-song";
+    everydaySong.textContent = `Songs that played everyday: ${checkEveryDaySongs(user)}`;
+    main.appendChild(everydaySong);
+    if (Object.keys(checkEveryDaySongs(user)).length === 0) {
+      everydaySong.remove();
+    }
+
+    const mostPlayedSong1 = document.createElement("p");
+    mostPlayedSong1.innerHTML = `The most played song (by number of listens): ${getMostPlayedSongByListens(user)}`;
+
+    const mostPlayedSong2 = document.createElement("p");
+    mostPlayedSong2.innerHTML = `The most played song (by listening time): ${getMostPlayedSongByTime(user)}`;
+
+    const mostPlayedArtist1 = document.createElement("p");
+    mostPlayedArtist1.innerHTML = `The most played artist (by number of listens): ${getMostPlayedArtistByListens(user)}`;
+
+    const mostPlayedArtist2 = document.createElement("p");
+    mostPlayedArtist2.innerHTML = `The most played artist (by listening time): ${getMostPlayedArtistByTime(user)}`;
+
+    const songFriday1 = document.createElement("p");
+    songFriday1.innerHTML = `The most played song on Friday night (by number of listens): ${getSong(getMostPlayedSongFridayByListens(user)).title}`;
+
+    const songFriday2 = document.createElement("p");
+    songFriday2.innerHTML = `The most played song on Friday night (by listening time): ${getSong(getMostPlayedSongFridayByTime(user)).title}`;
+
+    const mostPlayedGenres = document.createElement("p");
+    mostPlayedGenres.innerHTML = `${getMostGenresByListens(user).length} most played genres (by number of listens): ${getMostGenresByListens(user)}`;
+
+    const songInARow = document.createElement("p");
+    songInARow.innerHTML = `The most played song in a row: ${getSong(getSongInARow(user)[0]).title}, ${getSongInARow(user)[1]} times`;
+
+    main.append(
+      mostPlayedSong1,
+      mostPlayedSong2,
+      mostPlayedArtist1,
+      mostPlayedArtist2,
+      songFriday1,
+      songFriday2,
+      mostPlayedGenres,
+      songInARow,
+    );
+    if (getMostPlayedSongFridayByListens(user)===null) {songFriday1.remove()};
+    if (getMostPlayedSongFridayByTime(user)===null) {songFriday2.remove()};
+    if (getSongInARow(user) === ["No song played in a row", 0]) {songInARow.remove()};
+    document.body.appendChild(main);
+  }
+}
